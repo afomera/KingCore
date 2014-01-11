683e6c7f3434dad78a77b51bbf87b0618595a934
unknown <kaptoye@gmail.com>
2014-01-11 15:33:10 -0600

    Version bump, and commenting code

--- src/main/java/com/king601/KingCore/KingCoreListener.java
+++ src/main/java/com/king601/KingCore/KingCoreListener.java
@@ -1,18 +1,17 @@
 package com.king601.KingCore;
 
 
-import org.bukkit.Material;
+import org.bukkit.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.weather.WeatherChangeEvent;
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
 
 /**
  * User: king601
@@ -70,15 +69,50 @@ public class KingCoreListener implements Listener {
            player.sendMessage(ChatColor.DARK_PURPLE + "Welcome back to the Server!");
         }
     }
-
+    //This checks if Player's Item in Hand is Stone Hoe, if so Send em a message and cancel event
     @EventHandler
     public void handleInteract(PlayerInteractEvent event) {
         Player player = event.getPlayer();
 
         if(player.getItemInHand().getType()== Material.STONE_HOE) {
-
+            //now I know the player has a Hoe in hand.. let's make it do fun stuff.
             player.sendMessage("Hey there");
+            event.setCancelled(true);
         }
     }
+    //Tree Falling part, checks block above one you use an Axe on, if returns wood type then break it and all above
+    @EventHandler
+    public void treeBreak (BlockBreakEvent event) {
+        Material block = event.getBlock().getType();
+        Player player = event.getPlayer();
+        if (block == Material.LOG && player.getItemInHand().getType().name().toLowerCase().contains("axe")){
+           Location blockLocation = event.getBlock().getLocation();
+            double y = blockLocation.getBlockY();
+            double x = blockLocation.getBlockX();
+            double z = blockLocation.getBlockZ();
+
+            World currentWorld = event.getPlayer().getWorld();
+
+            boolean logsLeft = true;
+
+            while(logsLeft == true) {
+                y++ ; //Increse Y Cord.
+                Location blockAbove = new Location(currentWorld, x, y, z);
+                Material blockAboveType = blockAbove.getBlock().getType();
+                Byte blockAboveData = blockAbove.getBlock().getData();
+                if (blockAboveType == Material.LOG) {
+                    ItemStack droppedItem = new ItemStack(blockAboveType, 1, blockAboveData);
+                    currentWorld.playEffect(blockAbove, Effect.MOBSPAWNER_FLAMES, 1);
+                    blockAbove.getBlock().setType(Material.AIR);
+                    currentWorld.dropItem(blockAbove, droppedItem);
+                    logsLeft = true;
+                } else {
+                    logsLeft = false;
+                }
+
+            }
+        }
+    }
+    //More Handlers go below me
 
 }
